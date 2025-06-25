3266c59189f87b11776ed6ca82077d40316e07d8
Lili <Liliia_Nel@proton.me>
2025-06-25 23:35:20 +0200

    set up

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+const fs = require("node: fs/promises");
 
-export const readContacts = async () => {};
+
+export const readContacts = async () => {
+   try { 
+    const res = await fs.readFile(PATH_DB, {encoding: "utf-8"});
+    return res;
+} catch (error) {
+console.error(error);
+}
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+const fs = require("node: fs/promises");
 
-export const writeContacts = async (updatedContacts) => {};
+
+export const writeContacts = async (updatedContacts) => {
+    try {
+        const data = await fs.writeFile(PATH_DB, updatedContacts, { encoding: 'utf-8' });
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+};
