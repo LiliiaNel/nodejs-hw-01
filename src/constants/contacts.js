6e17157f7f2bd1f6cee94e809929e4edf8cae565
Lili <Liliia_Nel@proton.me>
2025-06-28 22:07:21 +0200

    fix path

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,5 +1,8 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export const PATH_DB = path.join('db', 'db.json');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export const PATH_DB = path.join(__dirname, '..','db', 'db.json');
 
 
